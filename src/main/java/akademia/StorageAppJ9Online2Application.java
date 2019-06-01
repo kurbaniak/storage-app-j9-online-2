ad2f05683bff961b08c70657013dd04b1f41e804
gucio <kurbaniak@gmail.com>
2019-06-01 12:10:26 +0200

    dodanie dostepu do bazy danych

--- src/main/java/akademia/StorageAppJ9Online2Application.java
+++ src/main/java/akademia/StorageAppJ9Online2Application.java
@@ -2,7 +2,9 @@ package akademia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:dbconfig.properties")
 @SpringBootApplication
 public class StorageAppJ9Online2Application {
 
