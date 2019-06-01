c1922f43913a55c4a10f1b7766d19a787622f17b
gucio <kurbaniak@gmail.com>
2019-06-01 10:58:09 +0200

    initial commit

---
+++ src/main/java/akademia/StorageAppJ9Online2Application.java
@@ -0,0 +1,13 @@
+package akademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageAppJ9Online2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageAppJ9Online2Application.class, args);
+    }
+
+}
